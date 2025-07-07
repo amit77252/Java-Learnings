a8daf6b1595cc1a614e02d8f4988f6693c6c714e
amitsingh <amitsingh@amits-mbp.lan>
2025-07-07 10:41:52 +0530

    User data reading & printing commit

--- JavaLearning/src/com/javaLearnings/DemoMethods1.java
+++ JavaLearning/src/com/javaLearnings/DemoMethods1.java
@@ -1,9 +1,21 @@
+//Program to read & display user details
 package com.javaLearnings;
-
+import java.util.Scanner;
 public class DemoMethods1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter the User Name:");
+		String name = s.nextLine(); // Read the String Data
+		System.out.println("Enter the Mail Id:");
+		String mail = s.nextLine();
+		System.out.println("Enter the Phone No:");
+		long phone = s.nextLong();
+		System.out.println("====User Details====");
+		System.out.println("User Name :"+name);
+		System.out.println("User Mail Id :"+mail);
+		System.out.println("User Phone No :"+phone);
+		
 
 	}
 
