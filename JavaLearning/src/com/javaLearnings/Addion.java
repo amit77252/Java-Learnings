1da43c5ae2aa5eb7d077cb50f8ebbd5f881b9155
amitsingh <amitsingh@amits-mbp.lan>
2025-07-13 14:21:48 +0530

    Going ahead

---
+++ JavaLearning/src/com/javaLearnings/Addion.java
@@ -0,0 +1,16 @@
+package com.javaLearnings;
+
+public class Addion {
+
+	public static void main(String[] args) {
+		
+		int a,b,c;
+		a=12;
+		b=9;
+		c= a+b;
+		System.out.println("Value of a:"+a);
+		System.out.println("Value of b:"+b);
+		System.out.println("Sum ="+c);
+	}
+
+}
---
+++ JavaLearning/src/com/javaLearnings/DemoAverage.java
@@ -0,0 +1,17 @@
+package com.javaLearnings;
+
+public class DemoAverage { //MainClass program to show average of 3 Nos
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=12;
+		b=12;
+		c=12;
+		int avg = (a+b+c)/3;
+		System.out.println("The Value a:"+a);
+		System.out.println("The Value b:"+b);
+		System.out.println("The Value c:"+c);
+		System.out.println("The Average:"+avg);
+	}
+
+}
