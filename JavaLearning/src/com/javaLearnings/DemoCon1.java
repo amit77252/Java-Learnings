ae350953b00b3e27f2dad14c6014836107c04b48
amit77252 <amitsingh.may1988@gmail.com>
2025-07-27 21:42:55 +0530

    Constructor program commit

--- JavaLearning/src/com/javaLearnings/DemoCon1.java
+++ JavaLearning/src/com/javaLearnings/DemoCon1.java
@@ -1,10 +1,22 @@
 package com.javaLearnings;
 //Program to use constructor syntax
-
+class CTest1{
+	int a = 10;
+	CTest1(){
+		System.out.println("==== Constructor CTest1() ====");
+		System.out.println("The value a: "+a);
+	}
+	void display() {
+		System.out.println("==== Instance Method display() ====");
+		System.out.println("The value a: "+a);
+	}
+}
 public class DemoCon1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		CTest1 ob = new CTest1(); // Constructor Call (Can be called only once while object creation
+		ob.display(); // calling the method
+		ob.display(); // multiple times we can call the method in java
 
 	}
 
