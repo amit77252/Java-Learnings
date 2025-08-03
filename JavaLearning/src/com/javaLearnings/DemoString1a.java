defab1f5c93b1575b153022d34e2a8e884df04fc
amit77252 <amitsingh.may1988@gmail.com>
2025-08-03 09:30:42 +0530

    Changed class name

---
+++ JavaLearning/src/com/javaLearnings/DemoString1a.java
@@ -0,0 +1,32 @@
+package com.javaLearnings;
+/* Program to input string, retrieve character based on Index and find out
+ * the character is vowel or consonent
+ */
+import java.util.*;
+public class DemoString1a {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str = s.nextLine();
+		int len = str.length();
+		System.out.println("Enter the Index of String: ");
+		int index = s.nextInt();
+		if(index>=0 && index<len) {
+			char ch = str.charAt(index);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u'
+				||ch=='A' ||ch=='E' ||ch=='I' ||ch=='O' ||ch=='U') {
+				
+				System.out.println("The Character "+ch+" is a Vowel");
+			}
+			else {
+				System.out.println("The Character "+ch+" is a Consonent");
+			}
+		}
+		else {
+			System.out.println("Entered Index is Invalid...");
+		}
+
+	}
+
+}
--- JavaLearning/src/com/javaLearnings/DemoString2.java
+++ JavaLearning/src/com/javaLearnings/DemoString2.java
@@ -1,31 +1,9 @@
 package com.javaLearnings;
-/* Program to input string, retrive character based on Index and find out
- * the character is vowel or consonent
- */
-import java.util.*;
+
 public class DemoString2 {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the String:");
-		String str = s.nextLine();
-		int len = str.length();
-		System.out.println("Enter the Index of String: ");
-		int index = s.nextInt();
-		if(index>=0 && index<len) {
-			char ch = str.charAt(index);
-			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' ||ch=='u'
-				||ch=='A' ||ch=='E' ||ch=='I' ||ch=='O' ||ch=='U') {
-				
-				System.out.println("The Character "+ch+" is a Vowel");
-			}
-			else {
-				System.out.println("The Character "+ch+" is a Consonent");
-			}
-		}
-		else {
-			System.out.println("Entered Index is Invalid...");
-		}
+		// TODO Auto-generated method stub
 
 	}
 
