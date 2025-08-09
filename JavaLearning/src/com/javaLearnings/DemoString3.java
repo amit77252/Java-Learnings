07225ae2346cb16615285ba89c0a2c16e9abcdf8
amit77252 <amitsingh.may1988@gmail.com>
2025-08-09 17:52:02 +0530

    Switch case on string commit

--- JavaLearning/src/com/javaLearnings/DemoString3.java
+++ JavaLearning/src/com/javaLearnings/DemoString3.java
@@ -6,8 +6,61 @@ import java.util.*;
 public class DemoString3 {
 
 	public static void main(String[] args) {
-		
-
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		String str = s.nextLine();
+		int len = str.length();
+		System.out.println("Enter the index to retrieve char: ");
+		int index = s.nextInt();
+		if(index >=0 && index <=len-1) {
+			char ch = str.charAt(index);
+			int j = (int)ch;// TypeCasting char to int (ASCII)
+			if((j>=65 && j<=90) || (j>=97 && j<=122)){
+				switch(ch){
+				case 'a':
+				case 'A':
+					System.out.println("Character: "+ch);
+					System.out.println("Vowel...");
+					break;
+				
+				case 'e':
+				case 'E':
+					System.out.println("Character: "+ch);
+					System.out.println("Vowel...");
+					break;
+					
+				case 'i':
+				case 'I':
+					System.out.println("Character: "+ch);
+					System.out.println("Vowel...");
+					break;
+					
+				case 'o':
+				case 'O':
+					System.out.println("Character: "+ch);
+					System.out.println("Vowel...");
+					break;
+				
+				case 'u':
+				case 'U':
+					System.out.println("Character: "+ch);
+					System.out.println("Vowel...");
+					break;
+				default:
+					System.out.println("Character: "+ch);
+					System.out.println("Consonent...");
+					
+				} //End of switch
+			} //End of if
+			else {
+				System.out.println("Character: "+ch);
+				System.out.println("Others...");
+			}
+		}
+		else {
+			System.out.println("Invalid Index...");
+		}
+		s.close();
 	}
 
 }
